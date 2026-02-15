96014c4457eca8ebb59d4883493ddd148de15bf9
deepaksharma-2002 <deep011345@gmail.com>
2026-02-15 16:43:27 +0530

    Add only simple-docker-project

---
+++ simple-docker-project/src/main/java/com/docker/SimpleDockerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDockerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDockerProjectApplication.class, args);
+	}
+
+}
